0ab526f9a2c4e195bf4e6afd362ad8e1f608162c
Franklin Tamborello, PhD, CSP, CHFP <5951180+tamborello@users.noreply.github.com>
2022-02-16 12:15:01 -0600

    got an acceptably working initial version with a single, very simple test case

--- FuzzyBooth.py
+++ FuzzyBooth.py
@@ -1,6 +1,9 @@
 # Fuzzy Booth
 # Search a document for a substring, but with Levenshtein edit distance fuzzy-matching.
 
+# To do:
+# Splitting's all well and good, but I should probably do a bit more processing on Text and SearchTerm, such as to use a generally accepted tokenizer and work on the tokens.
+
 import logging
 from nltk.metrics.distance import edit_distance
 
@@ -9,11 +12,14 @@ def scale_criterion (criterion, scale, denominator=50):
     return criterion + (scale ** 1/denominator)
 
 
-def search_for_term (Text='', SearchTerm = '', Transpositions = True, SubstitutionCost = 0.9, Criterion = 2.5, CriterionScale = False):
+def search_for_term (Text='', SearchTerm='', Transpositions=True, SubstitutionCost=0.9, Criterion=2.5, CriterionScale=False):
     '''For a search term phrase (could be single- or multi-word), find whether or not it's present within the text, subject to fuzziness of Levenshtein edit distance. If it's present then for each instance found: return the text that was found, the word index of its first word, and the Levenshtein edit distance.'''
-    resultTemplate={'SearchTerm' : SearchTerm, 'Found' : False, 'Index' : None, 'Distance' : None}
+    resultTemplate={'SearchTerm' : SearchTerm, 'Found' : False, 'Index' : None, 'Edit Distance' : None}
     results = []
+    SearchTerm = SearchTerm.split(" ")
     SearchTermlen = len(SearchTerm)
+    Text = Text.split(" ")
+    TextLen = len(Text)
     # Maybe scale scoring to the number of words in the search term
     # if it's false then skip the next statement
     if not CriterionScale:
@@ -24,41 +30,47 @@ def search_for_term (Text='', SearchTerm = '', Transpositions = True, Substituti
     # A phrase could be multiword. If the first word is a single letter, then it might not matter much to the overall score of the phrase. So search for each word of the SearchTerm.
     try: 
         # If the search term is much longer than the text then punt
-        if len(Text) < SearchTermlen:
+        if TextLen < SearchTermlen:
             results.append(resultTemplate)
             return results
         # Loop through the text
-        for txt in range(0, len(Text) - (SearchTermlen-1)):
+        for txt in range(0, TextLen - (SearchTermlen-1)):
+            # Start saving the text in case we want to return it as a result
+            thisText = Text[txt]
             # Examine the text item against the search term
-            dist = edit_distance(SearchTerm[0], Text[txt], substitution_cost=SubstitutionCost, transpositions=Transpositions)
+            dist = edit_distance(SearchTerm[0], thisText, substitution_cost=SubstitutionCost, transpositions=Transpositions)
             # if the distance of the current word to the phrase's first word is acceptable, 
             if dist <= ScaledCriterion:
-                # Start saving the text in case we want to return it as a result
-                thisText = Text[txt]
                 # see if the subsequent SearchTerm words match the following text words 
                 for subseqSearchTerm in range(1, SearchTermlen):
-                    thisText = thisText + Text[txt + subseqSearchTerm]
+                    subseqText = Text[txt + subseqSearchTerm]
+                    thisText = thisText + subseqText
                     # JIC the last word checked just happened to be at the end of the text
-                    dist += edit_distance(SearchTerm[subseqSearchTerm], Text[txt + subseqSearchTerm], substitution_cost=SubstitutionCost, transpositions=Transpositions)
+                    dist += edit_distance(SearchTerm[subseqSearchTerm], subseqText, substitution_cost=SubstitutionCost, transpositions=Transpositions)
                 # The first SearchTerm met criterion and we've measured edit distances of the subsequent words. Does the phrase meet criterion?
                 # If we've reached the end of comparing the search term to the text
                 # and dist is still <= scaledCriterion
                 if dist <= ScaledCriterion:
                     # The phrase meets criterion
-                    thisResult = resultTemplate
+                    thisResult = resultTemplate.copy()
                     # Verbiage found
                     thisResult['Found'] = thisText
                     # index of verbiage found
                     thisResult['Index'] = txt
                     # distance of verbiage found
-                    thisResult['Distance'] = dist
+                    thisResult['Edit Distance'] = dist
                     results.append(thisResult)
-        # We've run to the end of the text, so return negative result
+        # We've run to the end of the text, 
         else:
-            results.append(resultTemplate)
-            return results
+            # If we have any results already then return those,
+            if len(results) > 0:
+                return results
+            # Else we never found the search term, so return the negative case
+            else:
+                results.append(resultTemplate)
+                return results
     except Exception as exc:
-        msg = "Error in search_for_term: " + str(exc) + ", Search Term: " + str(SearchTerm) + ", Search Term Length: " + str(SearchTermlen) + ", Search Term Iterator Value: " + str(subseqSearchTerm) + ", Text Length: " + str(len(Text))
+        msg = "Error in search_for_term: " + str(exc) + ", Search Term: " + str(SearchTerm) + ", Search Term Length: " + str(SearchTermlen) + ", Search Term Iterator Value: " + str(subseqSearchTerm) + ", Text Length: " + str(TextLen)
         logging.error(msg)
     return results
 
